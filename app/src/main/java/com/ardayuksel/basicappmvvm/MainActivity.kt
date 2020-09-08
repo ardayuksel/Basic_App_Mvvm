6bbe17b8c7c219d9f20a98ef1baf5193c053b37a
Arda Yüksel <ardayuksel10@gmail.com>
2020-09-08 08:04:32 +0300

    Initial commit

--- app/src/main/java/com/ardayuksel/basicappmvvm/MainActivity.kt
+++ app/src/main/java/com/ardayuksel/basicappmvvm/MainActivity.kt
@@ -3,20 +3,30 @@ package com.ardayuksel.basicappmvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), LifecycleOwner {
+/*
+* Created by ardayuksel
+*
+* Our project purpose is that looking the ViewModel's job.
+* Thank to ViewModel, when screen rotation is changed, ViewModel lifecycle will keep state values on UI
+* So, the value will continue from where it left off.
+*/
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        /* Defining viewmodel variable from MainViewModel class that we created */
         var viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
         tvCounter.text = viewModel.counter.toString()
 
+        /* Setting button events and calling methods from our viewmodel class */
         btnIncrease.setOnClickListener {
             viewModel.increaseCounter()
             tvCounter.text = viewModel.counter.toString()
--- app/src/main/java/com/ardayuksel/basicappmvvm/MainViewModel.kt
+++ app/src/main/java/com/ardayuksel/basicappmvvm/MainViewModel.kt
@@ -2,8 +2,14 @@ package com.ardayuksel.basicappmvvm
 
 import androidx.lifecycle.ViewModel
 
+/*
+* Created by ardayuksel
+*/
+
+/* Creating our viewmodel class with extending ViewModel */
 class MainViewModel: ViewModel() {
 
+    /* Define counter and its methods. It will used on activity class */
     var counter = 0
 
     fun increaseCounter(){
